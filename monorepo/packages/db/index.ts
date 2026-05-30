bc64c30f49f9df0ec97bee6d0db7edd3c9cb221f
MOHIT-IITP <codingbymohit@gmail.com>
2026-05-30 16:21:40 +0530

    DONE: auth testing is done

--- monorepo/packages/db/index.ts
+++ monorepo/packages/db/index.ts
@@ -1,16 +1,19 @@
 import postgres from "postgres";
-import { drizzle } from "drizzle-orm/postgres-js";
+
+import { drizzle }
+from "drizzle-orm/postgres-js";
 
 import * as schema
-from "./schema/user";
+from "./schema";
 
 const client =
-  postgres(process.env.DATABASE_URL!);
+  postgres(
+    process.env.DATABASE_URL!
+  );
 
 export const db =
-  drizzle(client, {
+  drizzle(client,{
     schema,
   });
 
-export * from "./schema/user";
-
+export * from "./schema";
